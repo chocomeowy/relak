fda1e7583688b3fe5a98ab3f5943516e8468033a
fahjin <yongfahjin@hotmail.com>
2021-08-08 10:55:29 +0800

    top bar working but not on top...

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -6,32 +6,36 @@ import Home from "./Home";
 const Topbar = () => {
   return (
     <>
-      <nav className="top-bar">
-        <div className="top-bar-left">
-          <ul className="dropdown menu" data-dropdown-menu>
-            <li className="menu-text">Site Title</li>
-            <li>
-              <Link to="/">Main</Link>
-            </li>
-            <li>
-              <Link to="/breathe">Breathe</Link>
-            </li>
-            <li>
-              <a href="#">Three</a>
-            </li>
-          </ul>
-        </div>
-        <div className="top-bar-right"></div>
-      </nav>
+      <div data-sticky-container>
+        <div class="small-12 sticky" data-sticky data-options="marginTop: 0">
+          <nav className="top-bar">
+            <div className="top-bar-left">
+              <ul className="dropdown menu" data-dropdown-menu>
+                <li className="menu-text">Site Title</li>
+                <li>
+                  <Link to="/">Main</Link>
+                </li>
+                <li>
+                  <Link to="/breathe">Breathe</Link>
+                </li>
+                <li>
+                  <a href="#">Three</a>
+                </li>
+              </ul>
+            </div>
+            <div className="top-bar-right"></div>
+          </nav>
 
-      <main>
-        <Route path="/">
-          <Home />
-        </Route>
-        <Route path="/breathe">
-          <Breathe />
-        </Route>
-      </main>
+          <main>
+            <Route path="/">
+              <Home />
+            </Route>
+            <Route path="/breathe">
+              <Breathe />
+            </Route>
+          </main>
+        </div>
+      </div>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+// Add Foundation to index.js
+import "foundation-sites/dist/css/foundation.min.css";
 
 ReactDOM.render(
   <Router>
