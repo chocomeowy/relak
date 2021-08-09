53c8e42b1d3bccf23ce5102d9faf2161af46698d
charlenetan <tanwsc@gmail.com>
2021-08-09 11:02:31 +0800

    navbar kinda works for now, not responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
 import Topbar from "./components/Topbar";
+import { Route, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import Breathe from "./components/breathing/Breathe";
+import Home from "./components/Home";
+
 function App() {
   const token = useSelector((state) => state.auth.token);
 
   return (
     <div className="App">
       <Topbar />
+      <main>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/breathe">
+          <Breathe />
+        </Route>
+      </main>
     </div>
   );
 }
--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,39 +1,34 @@
 import React from "react";
 import { Route, Link } from "react-router-dom";
-import Breathe from "./breathing/Breathe";
-import Home from "./Home";
+// import Breathe from "./breathing/Breathe";
+// import Home from "./Home";
 
 const Topbar = () => {
   return (
-    <>
-      <div data-sticky-container>
-        <div class="small-12 sticky" data-sticky data-options="marginTop: 0">
-          <nav className="top-bar">
-            <div className="top-bar-left">
-              <ul className="dropdown menu" data-dropdown-menu>
-                <li className="menu-text">Relak</li>
-                <li>
-                  <Link to="/">Main</Link>
-                </li>
-                <li>
-                  <Link to="/breathe">Breathe</Link>
-                </li>
-                <li>
-                  <a href="#">Three</a>
-                </li>
-              </ul>
-            </div>
-            <div className="top-bar-right"></div>
-          </nav>
+    <> 
+      {/* <div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
+        <button class="menu-icon" type="button" data-toggle="example-menu"></button>
+        <div class="title-bar-title">Menu</div>
+      </div> */}
 
-          <main>
-            <Route path="/">
-              <Home />
-            </Route>
-            <Route path="/breathe">
-              <Breathe />
-            </Route>
-          </main>
+      <div data-sticky-container>
+        <div class="top-bar" id="example-menu" data-sticky data-options="marginTop:0;">
+          <div class="top-bar-left">
+            <span class="top-bar-title">Relak</span>
+          </div>
+          <div class="top-bar-right">
+            <ul class="dropdown menu align-right" data-dropdown-menu>
+              <li>
+                <Link to="/">Main</Link>
+              </li>
+              <li>
+                <Link to="/breathe">Breathe</Link>
+              </li>
+              <li>
+                <a href="#">Three</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
