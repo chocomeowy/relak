c026b4da5c9d9f6550f8aa308ef3fd58e613fb56
fahjin <yongfahjin@hotmail.com>
2021-08-17 22:02:54 +0800

    navbar with mediaquery media hook change

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 
 React
 AntDesign CSS
+antd-mobile
 Redux (redux, react-redux)
 React Router
 Recharts
@@ -13,3 +14,8 @@ Styled components
 react-responsive-carousel
 react-awesome-button
 moment.js
+react-media-hook
+react-router-transition
+react-howler
+react-awesome-button
+framer-motion
--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,23 +1,28 @@
+import {
+  FormOutlined,
+  HeartOutlined,
+  HomeOutlined,
+  LineChartOutlined,
+  LoginOutlined,
+  LogoutOutlined,
+  QuestionCircleOutlined,
+  SmileOutlined,
+  SoundOutlined,
+  UserOutlined,
+  HomeTwoTone,
+} from "@ant-design/icons";
 import { Layout, Menu } from "antd";
+import { Icon, NavBar } from "antd-mobile";
 import React from "react";
 import { useDispatch } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
 import { logOutAction } from "../redux/ducks/accountAuth";
-import {
-  HomeOutlined,
-  LineChartOutlined,
-  HeartOutlined,
-  FormOutlined,
-  SoundOutlined,
-  QuestionCircleOutlined,
-  LogoutOutlined,
-  LoginOutlined,
-  UserOutlined,
-  SmileOutlined,
-} from "@ant-design/icons";
+import "antd-mobile/dist/antd-mobile.css";
+import { useMediaPredicate } from "react-media-hook";
 const { Header } = Layout;
 
 const Topbar = () => {
+  const biggerThan910 = useMediaPredicate("(min-width: 910px)");
   const token = localStorage.token;
   const history = useHistory();
   //console.log(token);
@@ -29,49 +34,104 @@ const Topbar = () => {
   };
   return (
     <>
-      <Header className="header" defaultselectedkeys={["1"]}>
-        <Menu theme="dark" mode="horizontal">
-          <Menu.Item key="1" icon={<HomeOutlined />}>
-            <Link to="/">Main</Link>
-          </Menu.Item>
-          <Menu.Item key="2" icon={<HeartOutlined />}>
-            <Link to="/breathe">Breathe</Link>
-          </Menu.Item>
-          <Menu.Item key="3" icon={<SoundOutlined />}>
-            <Link to="/listen">Listen</Link>
-          </Menu.Item>
-          <Menu.Item key="4" icon={<QuestionCircleOutlined />}>
-            <Link to="/gethelp">Get Help</Link>
-          </Menu.Item>
-          <Menu.Item key="10" icon={<SmileOutlined />}>
-            <Link to="/about">About us</Link>
-          </Menu.Item>
-          {token === undefined ? (
-            <>
-              <Menu.Item key="5" icon={<LoginOutlined />}>
-                <Link to="/login">Login</Link>
-              </Menu.Item>
-              <Menu.Item key="6" icon={<UserOutlined />}>
-                <Link to="/signup">Sign up</Link>
-              </Menu.Item>
-            </>
-          ) : (
-            <>
-              <Menu.Item key="7" icon={<FormOutlined />}>
-                <Link to="/journal">Journal</Link>
-              </Menu.Item>
-              <Menu.Item key="8" icon={<LineChartOutlined />}>
-                <Link to="/profile">Profile</Link>
-              </Menu.Item>
-              <Menu.Item key="9" icon={<LogoutOutlined />}>
-                <Link to="login" onClick={signOut}>
-                  Log Out
-                </Link>
-              </Menu.Item>
-            </>
-          )}
-        </Menu>
-      </Header>
+      {biggerThan910 ? (
+        <Header className="header" defaultselectedkeys={["1"]}>
+          <Menu theme="dark" mode="horizontal">
+            <Menu.Item key="1" icon={<HomeOutlined />}>
+              <Link to="/">Main</Link>
+            </Menu.Item>
+            <Menu.Item key="2" icon={<HeartOutlined />}>
+              <Link to="/breathe">Breathe</Link>
+            </Menu.Item>
+            <Menu.Item key="3" icon={<SoundOutlined />}>
+              <Link to="/listen">Listen</Link>
+            </Menu.Item>
+            <Menu.Item key="4" icon={<QuestionCircleOutlined />}>
+              <Link to="/gethelp">Get Help</Link>
+            </Menu.Item>
+            <Menu.Item key="10" icon={<SmileOutlined />}>
+              <Link to="/about">About us</Link>
+            </Menu.Item>
+            {token === undefined ? (
+              <>
+                <Menu.Item key="5" icon={<LoginOutlined />}>
+                  <Link to="/login">Login</Link>
+                </Menu.Item>
+                <Menu.Item key="6" icon={<UserOutlined />}>
+                  <Link to="/signup">Sign up</Link>
+                </Menu.Item>
+              </>
+            ) : (
+              <>
+                <Menu.Item key="7" icon={<FormOutlined />}>
+                  <Link to="/journal">Journal</Link>
+                </Menu.Item>
+                <Menu.Item key="8" icon={<LineChartOutlined />}>
+                  <Link to="/profile">Profile</Link>
+                </Menu.Item>
+                <Menu.Item key="9" icon={<LogoutOutlined />}>
+                  <Link to="login" onClick={signOut}>
+                    Log Out
+                  </Link>
+                </Menu.Item>
+              </>
+            )}
+          </Menu>
+        </Header>
+      ) : (
+        <NavBar
+          mode="dark"
+          leftContent={
+            <Link to="/">
+              <HomeTwoTone />
+            </Link>
+          }
+          rightContent={[
+            <Icon key="0" type="ellipsis">
+              <Menu>
+                <Menu.Item key="2" icon={<HeartOutlined />}>
+                  <Link to="/breathe">Breathe</Link>
+                </Menu.Item>
+                <Menu.Item key="3" icon={<SoundOutlined />}>
+                  <Link to="/listen">Listen</Link>
+                </Menu.Item>
+                <Menu.Item key="4" icon={<QuestionCircleOutlined />}>
+                  <Link to="/gethelp">Get Help</Link>
+                </Menu.Item>
+                <Menu.Item key="10" icon={<SmileOutlined />}>
+                  <Link to="/about">About us</Link>
+                </Menu.Item>
+                {token === undefined ? (
+                  <>
+                    <Menu.Item key="5" icon={<LoginOutlined />}>
+                      <Link to="/login">Login</Link>
+                    </Menu.Item>
+                    <Menu.Item key="6" icon={<UserOutlined />}>
+                      <Link to="/signup">Sign up</Link>
+                    </Menu.Item>
+                  </>
+                ) : (
+                  <>
+                    <Menu.Item key="7" icon={<FormOutlined />}>
+                      <Link to="/journal">Journal</Link>
+                    </Menu.Item>
+                    <Menu.Item key="8" icon={<LineChartOutlined />}>
+                      <Link to="/profile">Profile</Link>
+                    </Menu.Item>
+                    <Menu.Item key="9" icon={<LogoutOutlined />}>
+                      <Link to="login" onClick={signOut}>
+                        Log Out
+                      </Link>
+                    </Menu.Item>
+                  </>
+                )}
+              </Menu>
+            </Icon>,
+          ]}
+        >
+          Relak
+        </NavBar>
+      )}
     </>
   );
 };
