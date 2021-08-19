77d0d5a07ebceda86e596957244a37a99c2eed98
fahjin <yongfahjin@hotmail.com>
2021-08-19 16:29:27 +0800

    Remove extra word

--- src/components/Gethelp.js
+++ src/components/Gethelp.js
@@ -6,23 +6,31 @@ const Gethelp = () => {
   const data = [
     {
       title: "Talk to someone",
-      content: ["Samaritans of Singapore(24H): ", "Singapore Police Force (24H): "],
+      content: [
+        "Samaritans of Singapore(24H): ",
+        "Singapore Police Force (24H): ",
+      ],
       link: `https://stayprepared.sg/mymentalhealth/i-need-support-now/`,
       linkName: "More hotlines",
       link2: `tel:1767`,
       link2Name: "1767",
       link3: `tel:999`,
-      link3Name: "999"
+      link3Name: "999",
     },
     {
       title: "Chat with professionals",
-      content: [`Do you need someone to talk to?` ,`Chat with a dedicated, anonymous professional from CHAT`],
+      content: [
+        `Do you need someone to talk to?`,
+        `Chat with a dedicated professional from CHAT`,
+      ],
       link: `https://www.chat.mentalhealth.sg/`,
       linkName: "Chat here",
     },
     {
       title: "Seek Help",
-      content: [`Find a range of free and/or low cost counselling services here. It's better to seek help than to keep it all in.`],
+      content: [
+        `Find a range of free and/or low cost counselling services here. It's better to seek help than to keep it all in.`,
+      ],
       link: "https://blog.moneysmart.sg/healthcare/counselling-singapore-free-affordable/",
       linkName: "MoneySmart Article",
     },
@@ -30,9 +38,7 @@ const Gethelp = () => {
 
   return (
     <div>
-      <Title style={{ padding: "10px", textAlign: "center" }}>
-        help.
-      </Title>
+      <Title style={{ padding: "10px", textAlign: "center" }}>help.</Title>
 
       <List
         grid={{
@@ -52,15 +58,15 @@ const Gethelp = () => {
               title={<span style={{ fontSize: "2em" }}>{item.title}</span>}
               style={{ fontSize: "1.5em" }}
             >
-               <p>
-                 {item.content[0]} 
-               <a href={item.link2}>{item.link2Name}</a>
-               </p>
+              <p>
+                {item.content[0]}
+                <a href={item.link2}>{item.link2Name}</a>
+              </p>
 
-               <p>
-                 {item.content[1]}
-                 <a href={item.link3}>{item.link3Name}</a>
-                 </p>
+              <p>
+                {item.content[1]}
+                <a href={item.link3}>{item.link3Name}</a>
+              </p>
               <br />
               <a href={item.link}>{item.linkName}</a>
             </Card>
